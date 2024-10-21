cb73d5e766065fb03945ade72b2a6969c9149682
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-21 16:04:37 +0200

    printed an hello world on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Primi Passi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+</head>
+<body class="p-5">
+    <header>
+        <h1 class="fw-bold text-center mb-3 p-3"> Laravel Primi Passi</h1>
+    </header>
+    <main>
+        <p class="fs-3">HELLO WORLD</p>
+    </main>
+</body>
+</html>
